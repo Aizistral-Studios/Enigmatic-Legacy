23d47c33bab45359778bb91a49e593496a600886
Extegral <VictorShadow-Integral@yandex.ru>
2020-10-07 23:10:19 +0200

    Update 2.5.3
    Fixes #80

--- src/main/java/com/integral/enigmaticlegacy/EnigmaticLegacy.java
+++ src/main/java/com/integral/enigmaticlegacy/EnigmaticLegacy.java
@@ -192,7 +192,7 @@ public class EnigmaticLegacy {
 	public static SimpleChannel packetInstance;
 
 	public static final String MODID = "enigmaticlegacy";
-	public static final String VERSION = "2.5.2";
+	public static final String VERSION = "2.5.3";
 	public static final String RELEASE_TYPE = "Release";
 	public static final String NAME = "Enigmatic Legacy";
 
