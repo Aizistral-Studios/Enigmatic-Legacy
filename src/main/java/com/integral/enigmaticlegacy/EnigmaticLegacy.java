6702f0e9c19c9593430c0ef4ccdba531fd47d761
Extegral <VictorShadow-Integral@yandex.ru>
2021-03-17 17:05:11 +0100

    Update 2.11.2

--- src/main/java/com/integral/enigmaticlegacy/EnigmaticLegacy.java
+++ src/main/java/com/integral/enigmaticlegacy/EnigmaticLegacy.java
@@ -215,7 +215,7 @@ public class EnigmaticLegacy {
 	public static SimpleChannel packetInstance;
 
 	public static final String MODID = "enigmaticlegacy";
-	public static final String VERSION = "2.11.1";
+	public static final String VERSION = "2.11.2";
 	public static final String RELEASE_TYPE = "Release";
 	public static final String NAME = "Enigmatic Legacy";
 
