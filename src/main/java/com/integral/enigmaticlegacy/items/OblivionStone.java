ce12e3c77568d991bc93ac24b5ce12c6b501bee0
Aizistral <VictorShadow-Integral@yandex.ru>
2022-01-22 23:03:47 +0100

    Fix #244
    Fixes #244

--- src/main/java/com/integral/enigmaticlegacy/items/OblivionStone.java
+++ src/main/java/com/integral/enigmaticlegacy/items/OblivionStone.java
@@ -113,7 +113,7 @@ public class OblivionStone extends ItemBase implements Vanishable {
 							ItemStack displayStack;
 							displayStack = new ItemStack(something, 1);
 
-							list.add(new TextComponent(" - ").append(((TextComponent)displayStack.getHoverName()).withStyle(ChatFormatting.GOLD)).withStyle(ChatFormatting.GOLD));
+							list.add(new TextComponent(" - " + displayStack.getHoverName().getString()).withStyle(ChatFormatting.GOLD));
 						}
 						counter++;
 					}
