379a112e8718c776df1cff9b4ec0775eb41393bc
Aizistral <VictorShadow-Integral@yandex.ru>
2022-02-06 20:03:46 +0100

    Some progress on #170

--- src/main/java/com/integral/enigmaticlegacy/handlers/EnigmaticKeybindHandler.java
+++ src/main/java/com/integral/enigmaticlegacy/handlers/EnigmaticKeybindHandler.java
@@ -9,10 +9,13 @@ import com.integral.enigmaticlegacy.packets.server.PacketXPScrollKey;
 
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.ClientRegistry;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingJumpEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.network.PacketDistributor;
 
@@ -30,6 +33,8 @@ public class EnigmaticKeybindHandler {
 	public KeyMapping spellstoneAbilityKey;
 	public KeyMapping xpScrollKey;
 
+	private boolean spaceDown = false;
+
 	@OnlyIn(Dist.CLIENT)
 	public void registerKeybinds() {
 		this.enderRingKey = new KeyMapping("key.enderRing", GLFW.GLFW_KEY_I, "key.categories.enigmaticLegacy");
@@ -44,8 +49,18 @@ public class EnigmaticKeybindHandler {
 	@SubscribeEvent
 	@OnlyIn(Dist.CLIENT)
 	public void onKeyInput(TickEvent.ClientTickEvent event) {
-		if (event.phase != TickEvent.Phase.END || !Minecraft.getInstance().isWindowActive())
+		if (event.phase != TickEvent.Phase.START || !Minecraft.getInstance().isWindowActive() || Minecraft.getInstance().player == null)
 			return;
+
+		boolean spaceDown = Minecraft.getInstance().options.keyJump.isDown();
+		boolean jumpClicked = false;
+
+		if (this.spaceDown != spaceDown) {
+			this.spaceDown = spaceDown;
+			if (spaceDown) {
+				jumpClicked = true;
+			}
+		}
 
 		if (this.enderRingKey.consumeClick()) {
 			if (Minecraft.getInstance().isWindowActive()) {
@@ -61,6 +76,21 @@ public class EnigmaticKeybindHandler {
 			EnigmaticLegacy.packetInstance.send(PacketDistributor.SERVER.noArg(), new PacketSpellstoneKey(true));
 		} else if (this.spellstoneAbilityKey.consumeClick() && SuperpositionHandler.hasSpellstone(Minecraft.getInstance().player)) {
 			EnigmaticLegacy.packetInstance.send(PacketDistributor.SERVER.noArg(), new PacketSpellstoneKey(true));
+		} else if (jumpClicked) {
+			LocalPlayer player = Minecraft.getInstance().player;
+
+			if (!player.isInWater() && !player.isOnGround() && !player.isCreative() && !player.isSpectator()
+					&& SuperpositionHandler.hasCurio(player, EnigmaticLegacy.angelBlessing)) {
+				EnigmaticLegacy.packetInstance.send(PacketDistributor.SERVER.noArg(), new PacketSpellstoneKey(true));
+			}
+		}
+	}
+
+	@SubscribeEvent
+	@OnlyIn(Dist.CLIENT)
+	public void onLivingJump(LivingJumpEvent event) {
+		if (event.getEntityLiving() instanceof LocalPlayer player) {
+			//this.suppressNextJumpClick = true;
 		}
 	}
 
