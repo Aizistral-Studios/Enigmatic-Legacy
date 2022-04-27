ee10ee11c98a5d9eb503e0a68b779daa2756c8d8
Aizistral <VictorShadow-Integral@yandex.ru>
2022-04-27 02:12:48 +0200

    Build 2.21.1 coming right up

--- src/main/java/com/integral/enigmaticlegacy/EnigmaticLegacy.java
+++ src/main/java/com/integral/enigmaticlegacy/EnigmaticLegacy.java
@@ -268,7 +268,7 @@ import net.minecraftforge.registries.ObjectHolder;
 @Mod(EnigmaticLegacy.MODID)
 public class EnigmaticLegacy {
 	public static final String MODID = "enigmaticlegacy";
-	public static final String VERSION = "2.21.0";
+	public static final String VERSION = "2.21.1";
 	public static final String RELEASE_TYPE = "Release";
 	public static final String NAME = "Enigmatic Legacy";
 
