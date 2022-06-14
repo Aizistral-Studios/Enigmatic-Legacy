844d5129f6f70a91a1a501cf61f992bb996cdf22
Aizistral <VictorShadow-Integral@yandex.ru>
2022-06-14 19:06:52 +0200

    Build 2.24.0 incoming

---
+++ src/main/java/com/integral/enigmaticlegacy/items/ItemRecreator.java
@@ -0,0 +1,5 @@
+package com.integral.enigmaticlegacy.items;
+
+public class ItemRecreator {
+
+}
--- src/main/java/com/integral/enigmaticlegacy/packets/clients/PacketPermadeath.java
+++ src/main/java/com/integral/enigmaticlegacy/packets/clients/PacketPermadeath.java
@@ -28,6 +28,13 @@ public class PacketPermadeath {
 	}
 
 	public static void handle(PacketPermadeath msg, Supplier<NetworkEvent.Context> ctx) {
+		// TODO Increase network safety
+
+		// For now this logic is vulnerable to client hacks, since we rely on client
+		// willingly processing the disconnect. Ideally we should drop connection
+		// server-side if client refused to do it within some short span of time after
+		// the packet was sent.
+
 		ctx.get().enqueueWork(() -> EnigmaticLegacy.proxy.displayPermadeathScreen());
 		ctx.get().setPacketHandled(true);
 	}
