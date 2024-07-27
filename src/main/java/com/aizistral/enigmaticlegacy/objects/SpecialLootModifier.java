d9b2755c597a8b7e4fd373d3528714dbbdff188c
Aizistral <admin@aizistral.com>
2024-07-27 23:44:19 +0200

    Add Heart of the Earth to suspicious block drops

--- src/main/java/com/aizistral/enigmaticlegacy/objects/SpecialLootModifier.java
+++ src/main/java/com/aizistral/enigmaticlegacy/objects/SpecialLootModifier.java
@@ -1,19 +1,26 @@
 package com.aizistral.enigmaticlegacy.objects;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import javax.annotation.Nonnull;
 
 import org.jetbrains.annotations.NotNull;
 
+import com.aizistral.enigmaticlegacy.api.generic.SubscribeConfig;
 import com.aizistral.enigmaticlegacy.config.OmniconfigHandler;
 import com.aizistral.enigmaticlegacy.handlers.SuperpositionHandler;
 import com.aizistral.enigmaticlegacy.registries.EnigmaticItems;
+import com.aizistral.omniconfig.Configuration;
+import com.aizistral.omniconfig.wrappers.Omniconfig;
+import com.aizistral.omniconfig.wrappers.OmniconfigWrapper;
 import com.google.common.base.Suppliers;
+import com.google.common.collect.ImmutableList;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
@@ -26,6 +33,29 @@ import net.minecraftforge.common.loot.LootModifier;
 
 public class SpecialLootModifier extends LootModifier {
 	public static final Supplier<Codec<SpecialLootModifier>> CODEC = Suppliers.memoize(() -> RecordCodecBuilder.create(inst -> codecStart(inst).apply(inst, SpecialLootModifier::new)));
+	public static final List<ResourceLocation> SUSPICIOUS_TABLES = ImmutableList.of(
+			new ResourceLocation("minecraft", "archaeology/desert_pyramid"),
+			new ResourceLocation("minecraft", "archaeology/desert_well"),
+			new ResourceLocation("minecraft", "archaeology/ocean_ruin_cold"),
+			new ResourceLocation("minecraft", "archaeology/ocean_ruin_warm"),
+			new ResourceLocation("minecraft", "archaeology/trail_ruins_common"),
+			new ResourceLocation("minecraft", "archaeology/trail_ruins_rare")
+			);
+
+	public static Omniconfig.DoubleParameter earthHeartChance;
+
+	@SubscribeConfig(receiveClient = true)
+	public static void onConfig(OmniconfigWrapper builder) {
+		builder.pushPrefix("CustomLoot");
+
+		if (builder.config.getSidedType() != Configuration.SidedConfigType.CLIENT) {
+			earthHeartChance = builder
+					.comment("The chance for Heart of the Earth to be obtained from suspicious blocks (default is 3%).")
+					.getDouble("EarthHeartChance", 0.03);
+		}
+
+		builder.popPrefix();
+	}
 
 	protected SpecialLootModifier(LootItemCondition[] conditions) {
 		super(conditions);
@@ -61,6 +91,14 @@ public class SpecialLootModifier extends LootModifier {
 						if (SuperpositionHandler.isTheCursedOne(player)) {
 							generatedLoot.add(new ItemStack(EnigmaticItems.ASTRAL_FRUIT, 1));
 						}
+					}
+				}
+
+			if (OmniconfigHandler.isItemEnabled(EnigmaticItems.EARTH_HEART))
+				if (SUSPICIOUS_TABLES.stream().anyMatch(table -> table.equals(context.getQueriedLootTableId()))) {
+					if (context.getRandom().nextDouble() < earthHeartChance.getValue()) {
+						generatedLoot.clear();
+						generatedLoot.add(new ItemStack(EnigmaticItems.EARTH_HEART, 1));
 					}
 				}
 		}
